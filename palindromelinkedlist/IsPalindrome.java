7e1f326edd6ce9de257be2d04fc073a945351b23
wangshiqiang <wangshiqiang@bsmail.com>
2019-07-02 09:10:39 +0800

    Update

--- palindromelinkedlist/IsPalindrome.java
+++ palindromelinkedlist/IsPalindrome.java
@@ -48,7 +48,7 @@ public class IsPalindrome {
             stringBuffer.append(node.val + ",");
             node = node.next;
         }
-        
+
         node = head;
         ListNode newListHead = null;
         while (node != null) {
@@ -57,14 +57,14 @@ public class IsPalindrome {
             newListHead = node;
             node = tmp;
         }
-        
+
         StringBuffer stringBuffer1 = new StringBuffer();
         node = newListHead;
         while (node != null) {
             stringBuffer1.append(node.val + ",");
             node = node.next;
         }
-        if(stringBuffer.toString().equals(stringBuffer1.toString())){
+        if (stringBuffer.toString().equals(stringBuffer1.toString())) {
             return true;
         }
         return false;
