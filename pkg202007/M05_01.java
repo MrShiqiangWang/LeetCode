0cf66d5efd64fe93e7ef7586c945e327c0725cf3
MrShiqiangWang <jjackwang@foxmail.com>
2020-07-13 21:52:10 +0800

    20200713 Three Sum Number

--- pkg202007/M05_01.java
+++ pkg202007/M05_01.java
@@ -2,8 +2,22 @@ package pkg202007;
 
 public class M05_01 {
 	public int insertBits(int N, int M, int i, int j) {
+		char[] ns = Integer.toBinaryString(N).toCharArray();
+		char[] ms = Integer.toBinaryString(M).toCharArray();
 
-		System.out.println("hello");
-		return -1;
+		int size = ns.length > j ? ns.length : (i == 0 ? j + 1 : j);
+		char[] result = new char[size];
+		for (int k = 0; k < size; k++) {
+			if (k - (size - j)+1 >= 0 && k - (size - j)+1 < ms.length) {
+				result[k] = ms[k - (size - j)+1];
+			} else {
+				result[k] = ns[k - (size - ns.length)];
+			}
+		}
+		return Integer.parseUnsignedInt(new String(result), 2);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new M05_01().insertBits(1024, 19, 2, 6));
 	}
 }
