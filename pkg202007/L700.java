1e4975cbf8a3a58166d01b2d49bd2dd6ee8ecf71
MrShiqiangWang <jjackwang@foxmail.com>
2020-07-21 18:04:04 +0800

    202007211804

---
+++ pkg202007/L700.java
@@ -0,0 +1,21 @@
+package pkg202007;
+
+import utils.TreeNode;
+
+public class L700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (val == root.val) {
+                return root;
+            }
+            else if (val < root.val) {
+                root = root.left;
+            }
+            else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
---
+++ pkg202007/L720.java
@@ -0,0 +1,78 @@
+package pkg202007;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class L720 {
+
+    public static void main(String[] args) {
+        String[] words = { "w", "wo", "wor", "worl", "world" };
+        System.out.println(new L720().longestWord(words));
+    }
+
+    private static class Node {
+
+        boolean isRoot = false;
+        boolean isEnd = false;
+        Node[] nodes = new Node[26];
+
+        // 构建字典表
+        public static void buildDictionary(Node node, String word) {
+            char[] cs = word.toCharArray();
+            for (int i = 0; i < cs.length; i++) {
+                if (node.nodes[cs[i] - 'a'] == null) {
+                    node.nodes[cs[i] - 'a'] = new Node();
+                    node = node.nodes[cs[i] - 'a'];
+                }
+                else {
+                    node = node.nodes[cs[i] - 'a'];
+                }
+            }
+            node.isEnd = true;
+        }
+
+    }
+
+    public String longestWord(String[] words) {
+        Node node = new Node();
+        node.isRoot = true;
+        // 构建字典树
+        for (int i = 0; i < words.length; i++) {
+            Node.buildDictionary(node, words[i]);
+        }
+        TreeSet<String> result = new TreeSet<String>(new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+        StringBuilder builder = new StringBuilder();
+        longestWord(node, result, builder);
+        return result.iterator().hasNext() ? result.iterator().next() : null;
+    }
+
+    private void longestWord(Node node, TreeSet<String> result, StringBuilder builder) {
+        if (node.isRoot == false && node.isEnd == false) {
+            result.add(builder.toString().substring(0, builder.length() - 1));
+            return;
+        }
+        boolean hasNext = false;
+        for (int i = 0; i < 26; i++) {
+            if (node.nodes[i] != null) {
+                hasNext = true;
+                builder.append((char) (i + 'a'));
+                longestWord(node.nodes[i], result, builder);
+                builder.deleteCharAt(builder.length() - 1);
+            }
+        }
+        if (!hasNext) {
+            result.add(builder.toString());
+        }
+    }
+}
