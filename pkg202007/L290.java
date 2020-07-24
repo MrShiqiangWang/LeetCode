be3bd8a8c109c5bd1fba2d06ed7e909ce8e1c10e
MrShiqiangWang <jjackwang@foxmail.com>
2020-07-24 16:35:58 +0800

    20200724

---
+++ pkg202007/L290.java
@@ -0,0 +1,46 @@
+package pkg202007;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class L290 {
+	public boolean wordPattern(String pattern, String str) {
+
+		int[] pt = pattern(pattern.toCharArray());
+		int[] st = pattern(str.split(" "));
+		if (pt.length != st.length) {
+			return false;
+		}
+		for (int i = 0; i < pt.length; i++) {
+			if (pt[i] != st[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private int[] pattern(char[] charArray) {
+		Character[] result = new Character[charArray.length];
+		for (int i = 0; i < charArray.length; i++) {
+			result[i] = charArray[i];
+		}
+		return pattern(result);
+	}
+
+	private <T> int[] pattern(T[] split) {
+		Map<T, Integer> map = new HashMap<T, Integer>();
+		int counter = 0;
+		int[] result = new int[split.length];
+		for (int i = 0; i < split.length; i++) {
+			if (map.containsKey(split[i])) {
+				result[i] = map.get(split[i]);
+			} else {
+				result[i] = counter;
+				map.put(split[i], counter);
+				counter++;
+			}
+		}
+		return result;
+	}
+}
