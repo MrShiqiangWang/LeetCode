d3c330eeece526e3f98ad960634927776d9e52b5
MrShiqiangWang <jjackwang@foxmail.com>
2020-07-24 17:46:07 +0800

    202007241746

---
+++ pkg202007/L821.java
@@ -0,0 +1,37 @@
+package pkg202007;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L821 {
+	public int[] shortestToChar(String S, char C) {
+		char[] cs = S.toCharArray();
+		List<Integer> charIndexMap = new ArrayList<Integer>();
+		for (int i = 0; i < cs.length; i++) {
+			if (cs[i] == C) {
+				charIndexMap.add(i);
+			}
+		}
+		int v = 0;
+		int[] re = new int[cs.length];
+		for (int i = 0; i < cs.length;) {
+			int valueIndex = charIndexMap.get(v);
+			if (v == 0 && valueIndex >= i) {
+				re[i] = valueIndex - i;
+				i++;
+				continue;
+			} else if (valueIndex < i && v + 1 == charIndexMap.size()) {
+				re[i]=i-valueIndex;
+				i++;
+				continue;
+			}else if(i>valueIndex&&i<=charIndexMap.get(v+1)) {
+				re[i]=Math.min(i-valueIndex, charIndexMap.get(v+1)-i);
+				i++;
+				continue;
+			}else {
+				v++;
+			}
+		}
+		return re;
+	}
+}
